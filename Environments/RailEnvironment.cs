866a7666816b9638a45650c33d9cfd9d23d0925c
chaihuaTJ <46558155+chaihuaTJ@users.noreply.github.com>
2023-03-13 14:22:28 +0800

    Add project files.

---
+++ Environments/RailEnvironment.cs
@@ -0,0 +1,28 @@
+﻿using ABxM.Core.Environments;
+using Rhino.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICD.ABM.DigitalFutures22.Core.Environments
+{
+    public class RailEnvironment : SingleBrepEnvironment
+    {
+        public List<Brep> Rails;
+
+        public RailEnvironment(Brep brep, List<Brep> rails)
+        {
+            BrepObject = brep;
+            Rails = rails;
+        }
+
+        public RailEnvironment(Brep brep, List<Brep> rails, double spacing)
+        {
+            BrepObject = brep;
+            Rails = rails;
+            ComputeCurvatureField(spacing);
+        }
+    }
+}
